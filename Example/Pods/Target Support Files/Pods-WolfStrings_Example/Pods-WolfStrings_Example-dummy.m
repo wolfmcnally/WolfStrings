6a5c984f2cb156d9c4f6d9aa3b7a2ae0a9a02867
Wolf McNally <wolf@wolfmcnally.com>
2018-09-18 16:30:36 -0700

    Removed Pods directory.

--- Example/Pods/Target Support Files/Pods-WolfStrings_Example/Pods-WolfStrings_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WolfStrings_Example : NSObject
-@end
-@implementation PodsDummy_Pods_WolfStrings_Example
-@end
--- Example/Pods/Target Support Files/Pods-WolfStrings_Tests/Pods-WolfStrings_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WolfStrings_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_WolfStrings_Tests
-@end
