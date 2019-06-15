97b82065425b5382a7702062e86a486e52d5137f
Wolf McNally <wolf@wolfmcnally.com>
2019-06-15 10:57:47 -0700

    Updating for Linux.

--- Sources/WolfStrings/StringFloatPrecision.swift
+++ Sources/WolfStrings/StringFloatPrecision.swift
@@ -22,6 +22,8 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 
+import Foundation
+
 extension String {
     public init(value: Double, precision: Int) {
         let formatter = NumberFormatter()
